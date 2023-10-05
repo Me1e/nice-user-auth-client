f454baf20efb88283491e70320e143c61a06b68e
Me1e <mele0404@naver.com>
2023-10-05 11:55:33 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,22 @@
 import axios from 'axios';
 
 export default function Home() {
+  // const serverHost = 'https://api.viewup.smiledragon.dev';
   const serverHost = 'http://localhost:4000';
 
   const checkIdentity = () => {
     const wl = window.location;
     const redirectUrl = `${wl.protocol}//${wl.host}/result`;
-    const returnUrl = `${serverHost}/nice/decrypt?redirectUrl=${redirectUrl}`;
+    // const returnUrl = `${serverHost}/nice/decrypt/register`;
+    // const returnUrl = `${serverHost}/nice/decrypt/findId`;
+    const returnUrl = `${serverHost}/nice/decrypt/resetPassword`;
+    const id = 'string123';
 
     axios
       .post(`${serverHost}/nice/encrypt/`, {
         returnUrl,
+        redirectUrl,
+        id,
       })
       .then(
         ({
@@ -19,6 +25,8 @@ export default function Home() {
             data: { encodeData },
           },
         }) => {
+          // encodeData =
+          //   'AgAFQlI3NjVOw49CpLomhK/iV8ZPUUyl2nD7sSuTB7hYg26VSiq4DmT4eZMTBeDMGat60/QSwwS5jWFZAR8Esyl1tfy3bmEBgVKwOpFvqy8uDK2Oahahty2NoRaMX/sn6gYZaGHUcCfmHxeXyvclSUZN966Q89pVCJC8G+XBzdXzjiUUAnc0ifAV3eckmqnx13NwhA+OSh6/BaZbOMqkBi0p3lzlEVGBNl7fTAzcFsqlBUibs3nc3xJyfvnmfcdbFAZLRNuS/4x0lHEiG+Jzn1163hTgL4kScADOzIygjs01uGMV0gPus6HqzUPyBuBvIFtpuEXTxE4VdeAUCv6jMhegfuDJOBFQxer5xTBSHHxe09S5vR2VNw/vT7Lk7x4vTriEOM9/ZsFNNfrb5u0wvRb2xu+Ystwraixw83bV6eM0zJjEY9/TS5QwpqjrMn/ASWzXdoooMACZ2A8RZtoPZZHihO7qxGF+L+oYMuv5cJXsToRCFjAl6J5E8YPgQ7bQOuOurM632C95nc56kqTJ0iURcT2IfC2DkIbwky9vVVos0unkWPtBW5QkYaTEUf5wdh2Iw3ecSAMGNu5gSY4/uTIBJfKgDlorvMKekD7/mXb3ADXYW8DwM0C1pzWq3gtsiSbYB9eAneQ43deOUug4CW2YMR+ZaPNq';
           const form = document.createElement('form');
           form.action =
             'https://nice.checkplus.co.kr/CheckPlusSafeModel/checkplus.cb';
@@ -38,7 +46,10 @@ export default function Home() {
           form.submit();
         }
       )
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        alert('본인인증에 실패했습니다.');
+      });
   };
 
   return (
